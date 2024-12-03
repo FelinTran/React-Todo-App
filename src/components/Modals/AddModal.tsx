a666a4506473e03f5115cc412a9f7310429f8a82
dinhhuyhuy <tdhuy21@clc.fitus.edu.vn>
2024-12-04 02:51:38 +0700

    Using React Hook Form for AddModal

--- src/components/Modals/AddModal.tsx
+++ src/components/Modals/AddModal.tsx
@@ -1,13 +1,7 @@
-import React, { useState } from "react";
-import { getRandomColors } from "../../utils/getRandomColors";
+import React from "react";
 import { v4 as uuidv4 } from "uuid";
 import { formatDate } from "../../utils/formatDate";
-
-interface Tag {
-	title: string;
-	bg: string;
-	text: string;
-}
+import {useForm} from "react-hook-form";
 
 interface AddTaskProps {
 	isOpen: boolean;
@@ -17,66 +11,30 @@ interface AddTaskProps {
 }
 
 const AddModal = ({ isOpen, onClose, setOpen, handleAddTask }: AddTaskProps) => {
-	const initialTaskData = {
-		id: uuidv4(),
+
+	const generateDefaultValues = () => ({
+		id: uuidv4(), // Generate a new random ID
 		title: "",
 		description: "",
 		priority: "",
 		duedate: formatDate(new Date()),
-		image: "",
-		alt: "",
-		tags: [] as Tag[],
-	};
+		status: ""
+	});
 
-	const [taskData, setTaskData] = useState(initialTaskData);
-	const [tagTitle, setTagTitle] = useState("");
-
-	const handleChange = (
-		e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>
-	) => {
-		const { name, value } = e.target;
-		setTaskData({ ...taskData, [name]: value });
-	};
-
-	const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		if (e.target.files && e.target.files[0]) {
-			const reader = new FileReader();
-			reader.onload = function (e) {
-				if (e.target) {
-					setTaskData({ ...taskData, image: e.target.result as string });
-				}
-			};
-			reader.readAsDataURL(e.target.files[0]);
-		}
-	};
-
-	const handleAddTag = () => {
-		if (tagTitle.trim() !== "") {
-			const { bg, text } = getRandomColors();
-			const newTag: Tag = { title: tagTitle.trim(), bg, text };
-			setTaskData({ ...taskData, tags: [...taskData.tags, newTag] });
-			setTagTitle("");
-		}
-	};
+	const { register, handleSubmit, reset } = useForm({
+		defaultValues: generateDefaultValues(),
+	});
 
 	const closeModal = () => {
 		setOpen(false);
 		onClose();
-		setTaskData(initialTaskData);
+		reset(generateDefaultValues());
 	};
 
-	const handleSubmit = () => {
-		handleAddTask(taskData);
+	const onSubmit = (data: any) => {
+		handleAddTask(data);
 		closeModal();
 	};
-
-  // const handleSubmit = () => {
-  //   handleAddTask({
-  //     ...taskData,
-  //     duedate: new Date(taskData.duedate), // Parse string to Date object
-  //   });
-  //   closeModal();
-  // };
 
 	return (
 		<div
@@ -88,27 +46,24 @@ const AddModal = ({ isOpen, onClose, setOpen, handleAddTask }: AddTaskProps) => 
 				className="w-full h-full bg-black opacity-70 absolute left-0 top-0 z-20"
 				onClick={closeModal}
 			></div>
-			<div className="md:w-[30vw] w-[90%] bg-white rounded-lg shadow-md z-50 flex flex-col items-center gap-3 px-5 py-6">
+			<form
+				onSubmit={handleSubmit(onSubmit)}
+				className="md:w-[30vw] w-[90%] bg-white rounded-lg shadow-md z-50 flex flex-col items-center gap-3 px-5 py-6"
+			>
 				<input
 					type="text"
-					name="title"
-					value={taskData.title}
-					onChange={handleChange}
+					{...register("title", {required: "Title is required"})}
 					placeholder="Title"
 					className="w-full h-12 px-3 outline-none rounded-md bg-slate-100 border border-slate-300 text-sm font-medium"
 				/>
 				<input
 					type="text"
-					name="description"
-					value={taskData.description}
-					onChange={handleChange}
+					{...register("description")}
 					placeholder="Description"
 					className="w-full h-12 px-3 outline-none rounded-md bg-slate-100 border border-slate-300 text-sm font-medium"
 				/>
 				<select
-					name="priority"
-					onChange={handleChange}
-					value={taskData.priority}
+					{...register("priority")}
 					className="w-full h-12 px-2 outline-none rounded-md bg-slate-100 border border-slate-300 text-sm"
 				>
 					<option value="">Priority</option>
@@ -117,61 +72,18 @@ const AddModal = ({ isOpen, onClose, setOpen, handleAddTask }: AddTaskProps) => 
 					<option value="high">High</option>
 				</select>
 				<input
-          type="date"
-          name="duedate"
-          value={taskData.duedate}
-          onChange={handleChange}
-          placeholder="duedate"
-          className="w-full h-12 px-3 outline-none rounded-md bg-slate-100 border border-slate-300 text-sm"
-        />
-				<input
-					type="text"
-					value={tagTitle}
-					onChange={(e) => setTagTitle(e.target.value)}
-					placeholder="Tag Title"
+					type="date"
+					{...register("duedate")}
+					placeholder="Due Date"
 					className="w-full h-12 px-3 outline-none rounded-md bg-slate-100 border border-slate-300 text-sm"
 				/>
 				<button
-					className="w-full rounded-md h-9 bg-slate-500 text-amber-50 font-medium"
-					onClick={handleAddTag}
-				>
-					Add Tag
-				</button>
-				<div className="w-full">
-					{taskData.tags && <span>Tags:</span>}
-					{taskData.tags.map((tag, index) => (
-						<div
-							key={index}
-							className="inline-block mx-1 px-[10px] py-[2px] text-[13px] font-medium rounded-md"
-							style={{ backgroundColor: tag.bg, color: tag.text }}
-						>
-							{tag.title}
-						</div>
-					))}
-				</div>
-				<div className="w-full flex items-center gap-4 justify-between">
-					<input
-						type="text"
-						name="alt"
-						value={taskData.alt}
-						onChange={handleChange}
-						placeholder="Image Alt"
-						className="w-full h-12 px-3 outline-none rounded-md bg-slate-100 border border-slate-300 text-sm"
-					/>
-					<input
-						type="file"
-						name="image"
-						onChange={handleImageChange}
-						className="w-full"
-					/>
-				</div>
-				<button
+					type="submit"
 					className="w-full mt-3 rounded-md h-9 bg-black text-blue-50 font-medium"
-					onClick={handleSubmit}
 				>
 					Save
 				</button>
-			</div>
+			</form>
 		</div>
 	)
 };
